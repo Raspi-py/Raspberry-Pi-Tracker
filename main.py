501b92588174719cec9959c17620536dc7a95002
waff <mwafiez@gmail.com>
2018-05-28 01:33:47 +0700

    Added layout

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def getAddress(location, width, height, zoom):
     locationnospaces = quote_plus(location)
     # -- python 2
     # locationnospaces = urllib.quote_plus(location)
-    address = "http://maps.googleapis.com/maps/api/staticmap?center={0}&zoom={1}&size={2}x{3}&format=gif&sensor=false".format(locationnospaces, zoom, width, height)
+    address = "http://maps.googleapis.com/maps/api/staticmap?center={0}&zoom={1}&size={2}x{3}&format=gif&sensor=true".format(locationnospaces, zoom, width, height)
     return address
 
 def getMap(location, width, height, zoom):
@@ -56,23 +56,36 @@ def canvasClick(event):
 
 def main():
     # init window,location and map zoom
-    location    = "Malang, Indonesia"
-    width       = 640
+    location = "-7.9682132,112.6305035"
+    width       = 720
     height      = 480
-    zoom        = 18
+    zoom        = 16
         
     # init tkinter
     window = tkinter.Tk()
+    window.option_add('*font', ('Arial', 11, 'bold'))
     # set window title
-    window.title(location)
+    window.title("Travel Track (Beta)")
     # set minimum size
     window.minsize(width, height)
     # call getmap function
     mapimage = getMap(location, width, height, zoom)
-    canvas = tkinter.Canvas(window, width=width, height=height)
+    canvas = tkinter.Canvas(window, width=540, height=height)
     canvas.create_image(0, 0, image=mapimage, anchor=tkinter.NW)
     canvas.bind("<Button-1>",canvasClick)
-    canvas.pack()
+    canvas.pack(side="left", fill="y")
+
+    # button tracking
+    btn_track = tkinter.Button(text="Track", height=2,
+                               bd='1px', bg="#2962FF", fg="white", relief="solid")
+    btn_track.pack(anchor='n', fill='x', padx=6, pady=2)
+
+    # button exit
+    btn_exit = tkinter.Button(text="Exit", height=2,
+                              bd='1px', bg="#DD2C00", fg="white", relief="solid",
+                              command=window.destroy)
+    btn_exit.pack(anchor="e", fill='x', padx=6, pady=2)
+
 
     window.mainloop()
 
